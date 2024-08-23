f6ea530843c733546fc1ade8dd8c3de01f3409c7
bejjankisreekar <sreekarbejjanki@gmail.com>
2024-08-24 00:27:59 +0530

    Leetcode question

---
+++ python/0021_Merge_two_sorted_linkedlists.py
@@ -0,0 +1,8 @@
+class ListNode:
+    def __int__(self, data):
+        self.data = data
+        self.next = None
+
+class MergeSortedList:
+    def merge_operation(self, first_list, second_list):
+        dummy = ListNode()
